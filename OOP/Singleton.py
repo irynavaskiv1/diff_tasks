b82d2837ab6bf03fbc2bda8c3578d715be10f1fe
irynavaskiv <irynavaskiv875@gmail.com>
2020-12-25 14:23:20 +0200

    create Singleton pattern

---
+++ OOP/Singleton.py
@@ -0,0 +1,24 @@
+class Singleton:
+    obj = None # attribute for storing a singe copy
+
+    def __new__(cls, *args, **kwargs):
+        print(type(cls))
+        if cls.obj is None: # it will work once
+            print('creating an object')
+            cls.obj = object.__new__(cls, *args, **kwargs)
+        return cls.obj # will return Singleton
+
+
+    def __init__(self):
+        print('_init__', type(self))
+
+s1 = Singleton()
+print()
+s2 = Singleton()
+
+# <class 'type'>
+# creating an object
+# _init__ <class '__main__.Singleton'>
+#
+# <class 'type'>
+# _init__ <class '__main__.Singleton'>
