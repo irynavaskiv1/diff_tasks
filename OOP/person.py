8cbabb2cf76ad2fa45365e5c57191e8cdafa4220
iryna.vaskiv <Iryna.Vaskiv@spacelabs.com>
2021-01-18 16:14:12 +0200

    add OOP file inheritance

--- OOP/person.py
+++ OOP/person.py
@@ -16,26 +16,43 @@ class Person:
         return '[Person: {}, {}]'.format(self.name, self.pay)
 
 
+class Manager(Person):
+    def giveRaise(self, persent, bonus=.10):
+        # self.pay = int(self.pay * (1 + persent + bonus)) - неправильно -> копіпаст
+        Person.giveRaise(self, persent + bonus)
 
 
 if __name__ == '__main__':
     # тільки коли файл запускається для тестування
-    bob = Person('Bob Vaskiv')
-    sue = Person('Sue Vaskiv1', 'QA', 25)
-    jak = Person('Jak', job='Dev', pay=40)
-    print(bob)
-    print(sue)
-    print(bob.name, bob.job, bob.pay)
-    print(sue.name, sue.job, sue.pay)
-    print(jak.name, jak.job, jak.pay)
+    bob = Person('Bob Smith', 'QA', 20000)
+    sue = Person('Sue Jones', 'AQA', 25000)
+    jak = Person('Jak', job='Dev', pay=40000)
+    # print(bob)
+    # print(sue)
+    # print(bob.name, bob.job, bob.pay)
+    # print(sue.name, sue.job, sue.pay)
+    # print(jak.name, jak.job, jak.pay)
 
     print('----------------------------------')
-    print('Surname is ', bob.name.split()[-1])
+    # print('Surname is ', bob.name.split()[-1])
     sue.pay *= 1.1
-    print('Sue new pay is ', round(sue.pay, 5))
+    # print('Sue new pay is ', round(sue.pay, 5))
+
+    # print('----------------------------------')
+    # print(bob.last_name(), sue.last_name())
+    # print(sue.giveRaise(0.1))
+    # print(sue.pay)
+    # print(sue)
 
     print('----------------------------------')
-    print(bob.last_name(), sue.last_name())
-    print(sue.giveRaise(0.1))
-    print(sue.pay)
-    print(sue)
+    tom = Manager('Tom Jones', 'mng', 50000)
+    tom.giveRaise(.20)
+    print(tom.last_name())
+    print(tom)
+
+    print('-----All three-----')
+    # поліморфізм, залежність методу giveRaise до об*єкта до якого він відноситься
+    for obj in (bob, sue, tom):
+        obj.giveRaise(.10)
+        print(obj)
+
