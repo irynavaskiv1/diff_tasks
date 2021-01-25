13dfe03c1961b8313c512cde2d273ca8fdf11044
Iryna Vaskiv <Iryna.Vaskiv@enghouse.com>
2021-01-25 19:00:25 +0200

    add OOP db file

---
+++ OOP/makedb.py
@@ -0,0 +1,12 @@
+from OOP.person import Person, Manager
+import shelve
+
+
+bob = Person('Bob Smith', 'QA', 20000)
+sue = Person('Sue Jones', 'AQA', 25000)
+jak = Manager('Jak', pay=40000)
+
+db = shelve.open('persondb')
+for object in (bob, sue, jak):
+    db[object.name] = object
+db.close()
