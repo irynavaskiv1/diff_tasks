eb525c4be17bb81db5f2fbed89d380a17d941691
irynavaskiv <irynavaskiv875@gmail.com>
2021-03-07 13:16:30 +0200

    reloads operators

---
+++ reload_operators/number.py
@@ -0,0 +1,16 @@
+# reload __init__
+
+class Number:
+    def __init__(self, start):
+        self.data = start
+
+    def sub(self, other):
+        return Number(self.data - other)
+
+
+X = Number(5)
+print(X.data)
+
+Y = X.data
+Y = Y - 2
+print(Y)
