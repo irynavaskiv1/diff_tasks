f1c0c4d0b79673d960de048675dd77efb44017b6
irynavaskiv <irynavaskiv875@gmail.com>
2021-05-09 23:29:28 +0300

    decorator function

---
+++ OOP/decorator_function.py
@@ -0,0 +1,16 @@
+
+class tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args):
+        self.calls += 1
+        print(f'call {self.calls} to {self.func.__name__}')
+        self.func(*args)
+
+
+@tracer
+def spam(a, b, c):
+    print(a, b, c)
+
