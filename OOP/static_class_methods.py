31b6cfbf12af32df6e7ea3cfeba9645903713f1a
irynavaskiv <irynavaskiv875@gmail.com>
2021-05-09 22:34:20 +0300

    static class methods

---
+++ OOP/static_class_methods.py
@@ -0,0 +1,13 @@
+class Methods:
+
+    def imeth(self, x):
+        print(self, x)
+
+    def smeth(x):
+        print(x)
+        
+    def cmeth(cls, x):
+        print(cls, x)
+
+    smeth = staticmethod(smeth) # same like @staticmethod
+    cmeth = classmethod(cmeth) # same like @classmethod
