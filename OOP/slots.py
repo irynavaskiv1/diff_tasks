78699e1253fb141d27b772036009192aa78e0ca9
irynavaskiv <irynavaskiv875@gmail.com>
2021-05-09 22:26:51 +0300

    slots

---
+++ OOP/slots.py
@@ -0,0 +1,38 @@
+class Limiter:
+    __slots__ = ['age', 'name', 'job']
+
+
+x = Limiter()
+#x.age # AttributeError: age
+
+x.age = 24 #спочатку використати присвоєння, далі виклик
+print(x.age)
+
+#____________________________________________________________________
+
+class C:
+    __slots__ = ['a', 'b']
+
+y = C()
+y.a = 1
+
+# y.__dict__  AttributeError
+print(getattr(y, 'a')) # 1
+
+#____________________________________________________________________
+
+class D:
+    __slots__ = ['a', 'b', '__dict__']
+
+z = D()
+print(z.__dict__) #{}
+
+#____________________________________________________________________
+class E:
+    __slots__ = ['c', 'd']
+
+class R(E):
+    __slots__ = ['r', 'm', '__dict__']
+
+c = R()
+print(c.__dict__) # {}
