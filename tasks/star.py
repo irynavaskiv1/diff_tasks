cf3c9e051bed51a07f9f52c7dd6de09e2fa6fa57
irynavaskiv <irynavaskiv875@gmail.com>
2021-05-14 14:05:53 +0300

    relocate docs1

---
+++ tasks/star.py
@@ -0,0 +1,58 @@
+class Star:
+
+    def __init__(self):
+        self.colors = {'blue': [], 'white': [], 'yellow': [],
+                       'orange': [], 'red': []}
+        self.star_and_temp = dict()
+
+    def get_star_and_temp(self):
+        while True:
+            data = input('Enter star & temperature separated by ":" ')
+            if data == 'e':
+                break
+            self.name_temp = data.split(':')
+            self.star_and_temp[int(self.name_temp[1])] = self.name_temp[0]
+        print(self.star_and_temp)
+
+    def adding_planet(self):
+        # for _ in range(len(self.name_temp[1])):
+        for _ in range(len(self.star_and_temp.keys())):
+            if 0 < int(self.name_temp[1]) < 10:
+                if self.name_temp[0] in self.colors.get('white'):
+                    pass
+                else:
+                    self.colors.get('white').append(self.name_temp[0])
+
+            elif 10 < int(self.name_temp[1]) < 22:
+                if self.name_temp[0] in self.colors.get('blue'):
+                    pass
+                else:
+                    self.colors.get('blue').append(self.name_temp[0])
+
+            elif int(self.name_temp[1]) < 40:
+                if self.name_temp[0] in self.colors.get('yellow'):
+                    pass
+                else:
+                    self.colors.get('yellow').append(self.name_temp[0])
+
+            elif int(self.name_temp[1]) < 49:
+                if self.name_temp[0] in self.colors.get('orange'):
+                    pass
+                else:
+                    self.colors.get('orange').append(self.name_temp[0])
+
+            elif int(self.name_temp[1]) < 100:
+                if self.name_temp[0] in self.colors.get('red'):
+                    pass
+                else:
+                    self.colors.get('red').append(self.name_temp[0])
+
+            else:
+                print('I do know!')
+
+        print(self.colors)
+
+
+s = Star()
+s.get_star_and_temp()
+s.adding_planet()
---
+++ tasks/star_1.py
@@ -0,0 +1,36 @@
+class Star:
+
+    def __init__(self):
+        self.star_and_temp = dict()
+
+    def get_star_and_temp(self):
+        while True:
+            data = input('Enter star & temperature separated by ":" ')
+            if data == 'e':
+                break
+            name_temp = data.split(':')
+            self.star_and_temp[int(name_temp[1])] = name_temp[0]
+        print(self.star_and_temp)
+
+    def print_color_and_star(self):
+        color_and_stars = []
+        for temp in self.star_and_temp:
+            if int(temp) < 0:
+                color_and_stars.append({'white': self.star_and_temp[temp]})
+            elif 0 <= int(temp) < 22:
+                color_and_stars.append({'blue': self.star_and_temp[temp]})
+            elif 22 <= int(temp) < 40:
+                color_and_stars.append({'yellow': self.star_and_temp[temp]})
+            elif 40 <= int(temp) < 49:
+                color_and_stars.append({'orange': self.star_and_temp[temp]})
+            elif 49 <= int(temp) < 100:
+                color_and_stars.append({'red': self.star_and_temp[temp]})
+            else:
+                color_and_stars.append({'I do not number':
+                                            self.star_and_temp[temp]})
+        print(color_and_stars)
+
+
+s = Star()
+s.get_star_and_temp()
+s.print_color_and_star()
