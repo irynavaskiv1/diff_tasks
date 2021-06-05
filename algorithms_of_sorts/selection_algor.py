56522583b125620c1738c5c64abccced938ed062
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-06-05 16:32:23 +0300

    add selection_algor.py

---
+++ algorithms_of_sorts/selection_algor.py
@@ -0,0 +1,13 @@
+def selection_sort(nums):
+    #  i -> кількість відсортованих значень
+    for i in range(len(nums)):
+        lowest_value_index = i  # допустимо, що найменший елемент- перший
+        for j in range(i + 1, len(nums)):  # перебірка несортованих елементів
+            if nums[j] < nums[lowest_value_index]:
+                lowest_value_index = j
+        nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]  # найменший міняємо з першим елементом
+
+
+random_list_of_nums = [12, 8, 3, 20, 11]
+selection_sort(random_list_of_nums)
+print(random_list_of_nums)
