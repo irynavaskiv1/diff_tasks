aa76ff7256bda4f9427c7ec96b42bf6c9a79449c
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-07-01 17:36:21 +0300

    add leaner algor

---
+++ algorithms/lenear_search.py
@@ -0,0 +1,11 @@
+def lenear_search(input_list, x):
+    for i in range(len(input_list)):
+        if input_list[i] == x:
+            return i
+    return -1
+
+
+test_list = ['v', 'a', 's', 'k', 'i', 'v']
+find_value = 'k'
+
+print(lenear_search(test_list, find_value))
