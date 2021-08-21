d187c3c3d8a8e4fec9e6a3e5a52c75b785a1da75
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-08-21 22:19:11 +0300

    add stack logic

---
+++ data_structure/user_defined/stack_list.py
@@ -0,0 +1,17 @@
+# A stack is a linear data structure that stores items in a LIFO or FILO manner.
+
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
