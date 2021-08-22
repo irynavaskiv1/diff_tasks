832592d4137873992e67d44242901dbc406c65f7
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-08-22 15:37:46 +0300

    update algor logic

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -16,27 +16,16 @@ class BinarySearch:
         return None
 
     def search_recursive(self, list, low, high, item):
-        # Check base case
         if high >= low:
-
             mid = (high + low) // 2
             guess = list[mid]
-
-            # If element is present at the middle itself
             if guess == item:
                 return mid
-
-            # If element is smaller than mid, then it can only
-            # be present in left subarray
             elif guess > item:
                 return self.search_recursive(list, low, mid - 1, item)
-
-                # Else the element can only be present in right subarray
             else:
                 return self.search_recursive(list, mid + 1, high, item)
-
         else:
-            # Element is not present in the array
             return None
 
 
