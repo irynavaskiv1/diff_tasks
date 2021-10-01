10ed5b038cd1fe459505c3618ff65d5d75caaffa
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-01 21:01:14 +0300

    S solid principle

---
+++ python/solid/1S.py
@@ -0,0 +1,28 @@
+"""
+Single responsibility principle - Кожен об'єкт має виконувати лише один обов'язок. Клас має виконувати 1 дію.
+"""
+
+
+class IraName:
+
+    def __init__(self, name):
+        self.name = name
+
+    def return_name(self):
+        return self.name
+
+
+class IraSurname:
+
+    def __init__(self, surname):
+        self.surname = surname
+
+    def get_surname(self):
+        return self.surname
+
+
+n = IraName('irynka-kanfetka')
+print(n.return_name())
+
+m = IraSurname('va')
+print(m.get_surname())
