20b4251c53947c85e202bbfbe524afb19f30c03a
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-07 17:42:06 +0300

    add dependency inversion

---
+++ python/solid/5D.py
@@ -0,0 +1,30 @@
+"""
+Залежності всередині системи будуються на основі абстракцій, що не повинні залежати від деталей; навпаки,
+деталі мають залежати від абстракцій. Класи верхніх рівнів не мають залежати від класів нижчих рівнів.
+"""
+
+
+class NewsPerson:
+    """This is a high-level module"""
+    @staticmethod
+    def publish(news: str) -> None:
+        """
+        :param news:
+        :return:
+        """
+        print(NewsPaper().publish(news=news))
+
+
+class NewsPaper:
+    """This is a low-level module"""
+    @staticmethod
+    def publish(news: str) -> None:
+        """
+        :param news:
+        :return:
+        """
+        print(f"{news} Hello newspaper")
+
+
+person = NewsPerson()
+print(person.publish("News Paper"))
