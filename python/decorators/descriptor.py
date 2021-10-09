ba2a3f970cc4eb34f281d25beead57581dfbb777
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-09 09:35:59 +0300

    add descriptors implementation

---
+++ python/decorators/descriptor.py
@@ -0,0 +1,26 @@
+class Name:
+    def __get__(self, instance, owner):
+        print('fetch...')
+        return instance._name
+
+    def __set__(self, instance, value):
+        print('change...')
+        instance._name = value
+
+    def __delete__(self, instance):
+        print('remove...')
+        del instance._name
+
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+    name = Name()
+
+
+ira = Person('Ira Vaskiv')
+print(ira.name)
+
+ira.name = 'Ira Vaskiv'
+
+del ira.name
