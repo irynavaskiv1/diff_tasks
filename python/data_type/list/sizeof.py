15c11eee4f367978cfd4d4325077b1a74a0114e6
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-25 22:34:58 +0300

    update some examples

--- python/data_type/list/sizeof.py
+++ python/data_type/list/sizeof.py
@@ -1,7 +1,9 @@
+# use magic method
 a = [1, 2, 3]
-
 b = (1, 2, 3)
 print(a.__sizeof__())
 print(b.__sizeof__())
 
-d = dict.fromkeys()
+# use method
+d = dict.fromkeys(a)
+d2 = dict.fromkeys(b)
--- python/data_type/list/slicing.py
+++ python/data_type/list/slicing.py
@@ -1,8 +1,3 @@
-a = 3
-print(id(a))
-a = 5
-print(id(a))
-
 list1 = [1, 3, 4]
 print(id(list1))
 list1[0] = 33
