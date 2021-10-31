6f7e72391a215fb58e2ebd6daf4115bce535caf4
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-10-31 22:19:06 +0200

    add inheritance example

---
+++ python/OOP/2_inheritance.py
@@ -0,0 +1,46 @@
+class ParentClass:
+    DEFAULT_VAR = 5
+
+    def __init__(self):
+        self.title = "IRA"
+
+    def parent_method(self):
+        print(self.parent_method.__name__)
+
+    def hello(self):
+        print(f"Hello from {self.__class__.__name__}")
+
+
+class SubClass(ParentClass):
+
+    def __init__(self):
+        self.title = "IRASUB"
+
+    def show_value(self):
+        print(self.DEFAULT_VAR)
+
+    def hello(self):
+        print(f"Hello from {self.__class__.__name__}")
+
+
+class SubClass2(SubClass):
+
+    def __init__(self):
+        self.title = "IRASUB2"
+
+    def show_value(self):
+        print(self.DEFAULT_VAR)
+
+    def hello(self):
+        print(f"Hello from {self.__class__.__name__}")
+
+
+# дерево наслідування, від чого наслідується клас
+print(SubClass.__mro__)
+# (<class '__main__.SubClass2'>, <class '__main__.SubClass'>, <class '__main__.ParentClass'>, <class 'object'>)
+
+# дерево наслідування, які класи наслідуються від класу
+print(SubClass.__subclasses__())
+
+s = SubClass2()
+print(super(SubClass2, s))
