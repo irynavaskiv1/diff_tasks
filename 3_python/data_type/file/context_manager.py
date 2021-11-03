58acdad80e025b5790e916bb3ebe10c381f7ad2d
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-11-03 11:14:22 +0200

    add cover contex manager

--- 3_python/data_type/file/context_manager.py
+++ 3_python/data_type/file/context_manager.py
@@ -1,3 +1,12 @@
-myfile = open(r'')
+# for files
 
-#TODO: add implementing
+f = open('filename.txt', 'w')
+f.write('sometext')
+f.flush()
+f.close()
+
+# same as, doing flush and close by himself
+with open('filename.txt', 'w') as file:
+    """ use __enter__ and __exit__ magic methods"""
+    file.write('sometext')
+
