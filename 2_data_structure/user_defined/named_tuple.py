f4a7dcb2dbe5f31957efa7288b3838fd5beb9157
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-11-04 18:51:38 +0200

    add named tuple

---
+++ 2_data_structure/user_defined/named_tuple.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+
+
+class ProtectedC:
+    __slots__ = ['age']   # what we can change, is not dict exactly
+    name = "Jim"
+
+
+pt = ProtectedC()
+ProtectedC = namedtuple('Service', ('name', 'url', 'ip_attr'))
+protected1 = ProtectedC(name='name3', url='url3', ip_attr='ip_attr3')
+print(protected1)
+
+service = namedtuple('Service', ('name', 'url', 'ip_attr'))
+service1 = service(name='name1', url='url1', ip_attr='ip_attr1')
+service2 = service(name='name2', url='url2', ip_attr='ip_attr2')
+print(service2)
+print(service2.name)
+
+
+new_pt = pt._asdict()
