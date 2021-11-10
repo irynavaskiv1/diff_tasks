9c67145f1f9a7103d517ef41abe994c7365f2fee
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-11-10 11:20:36 +0200

    add meta class

---
+++ 3_python/OOP3/meta_class.py
@@ -0,0 +1,32 @@
+class Dot:
+    class_var_dot = 11
+
+    def __init__(self, new_var):
+        self.new_var = new_var
+
+
+class Dot2(Dot):
+    class_var_dot2 = 22
+
+    def __init__(self, new_var2):
+        self.new_var2 = new_var2
+
+
+dot = Dot('.')
+print(type(dot))
+
+type(Dot)
+print(isinstance(Dot, type))
+
+
+body = """def __init__(self, new_var2):" \
+       self.new_var2 = new_var2
+       """
+cls_dict = type.__prepare__("Dot2", (Dot,))
+print(cls_dict)
+
+
+exec(body, globals(), cls_dict)
+print(cls_dict)
+
+Dot2 = type('Dot2', (Dot,), cls_dict)
