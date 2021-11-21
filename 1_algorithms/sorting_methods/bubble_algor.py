eb3c1160d09d0f475309f808750718e94b97ad5f
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-11-22 00:09:11 +0200

    add description to sorting algor

--- 1_algorithms/sorting_methods/bubble_algor.py
+++ 1_algorithms/sorting_methods/bubble_algor.py
@@ -1,4 +1,8 @@
 def bubble_algor(nums):
+    """
+    Is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if
+    they are in the wrong order.
+    """
     flag = True
     while flag:
         flag = False
