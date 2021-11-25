16189850291f944ce8bb041ac1c1ab17cda752f2
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-11-25 10:20:22 +0200

    add parcer pickle

---
+++ 3_python/tasks/parser_pickle.py
@@ -0,0 +1,17 @@
+import pickle
+
+data = {1: '1', 2: 'two'}
+file = 'file_pickle.pc'
+
+
+with open(file, 'wb') as f:
+    str_repr = pickle.dumps(data)
+    print(str_repr)
+    pickle.dump(data, file=f)
+
+
+with open(file, 'rb') as f:
+    data1 = pickle.load(file=f)
+    data2 = pickle.loads(str_repr)
+
+print(data1, data2)
