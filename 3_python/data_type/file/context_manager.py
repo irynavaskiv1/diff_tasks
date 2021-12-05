149fa6e9021284e25067d4097ba1ab70f96a0819
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2021-12-05 15:41:48 +0200

    add files context_manager

--- 3_python/data_type/file/context_manager.py
+++ 3_python/data_type/file/context_manager.py
@@ -6,7 +6,6 @@ f.flush()
 f.close()
 
 # same as, doing flush and close by himself
-with open('filename.txt', 'w') as file:
+with open('filename_second.txt', 'w') as file:
     """ use __enter__ and __exit__ magic methods"""
-    file.write('sometext')
-
+    file.write('sometext_second')
