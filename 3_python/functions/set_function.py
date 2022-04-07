97b789bd8bf2d0ee0a9ee1eafa2f86f8c4500775
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-04-07 12:11:38 +0300

    update functions block

--- 3_python/functions/set_function.py
+++ 3_python/functions/set_function.py
@@ -2,15 +2,14 @@ x = set()
 
 x.add(1)
 print(x)
+
 x.add(2)
 print(x)
 
 x.remove(2)
-
 print(x)
 
 x.remove(2)
-
 print(x)
 
 
@@ -19,7 +18,6 @@ print(x)
 
 
 x.clear()
-
 print(x)
 
 
@@ -27,5 +25,4 @@ ira = set()
 
 
 ira.add(100)
-
 print(len(ira))
