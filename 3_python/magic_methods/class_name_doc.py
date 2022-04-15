a54d23fe640f08a6638d9983ca64413f9625af17
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-04-15 15:18:36 +0300

    add class_name_doc.py

---
+++ 3_python/magic_methods/class_name_doc.py
@@ -0,0 +1,15 @@
+class MyClass:
+    """ Class Docstring """
+    MY_VAR = 10
+
+    def method(self):
+        self.__class__.MY_VAR = 20
+        print('self.method.__name__', self.method.__name__)
+        print('self.method.__doc__', self.method.__doc__)
+
+        print('self.__class__.__name__', self.__class__.__name__)
+        print('self.__class__.__doc__', self.__class__.__doc__)
+
+
+m = MyClass()
+m.method()
