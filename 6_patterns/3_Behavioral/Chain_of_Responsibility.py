9ba41be3c0dec754b797fec366021ab4664c9a94
iryna.vaskiv <Iryna.Vaskiv@enghouse.com>
2022-06-03 14:51:50 +0300

    little fixes

--- 6_patterns/3_Behavioral/Chain_of_Responsibility.py
+++ 6_patterns/3_Behavioral/Chain_of_Responsibility.py
@@ -7,7 +7,7 @@ from typing import Any, Optional
 class Handler(ABC):
 
     @abstractmethod
-    def set_next(self, handler: Handler) -> Handler:
+    def set_next(self, handler):
         pass
 
     @abstractmethod
